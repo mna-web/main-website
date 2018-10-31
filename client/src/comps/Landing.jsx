5af61a4d81682e2945238ca89c55623c1cc683f1
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-10-31 01:53:58 -0700

    expands row heights on landing. adds stock photos.

--- client/src/comps/Landing.jsx
+++ client/src/comps/Landing.jsx
@@ -28,28 +28,28 @@ class Landing extends Component {
 
     return (
       <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
-          <div className="container">
+        <div className="landing-inner text-light">
+          <div className="container-fluid">
             <div className="row">
-              <div className="col-md-12 text-center">
+              <div className="col-lg-12 text-center landing-row1">
                 <h1 className="display-3 mb-4">MNA Web Services</h1>
                 <p className="lead"> Creating high quality, dynamic web content </p>
               </div>
             </div>
             <div className="row">
-              <div className="col-md-12 text-center">
+              <div className="col-md-12 text-center landing-row2">
                 <h1 className="display-3 mb-4">SECOND ROW</h1>
                 <p className="lead"> second row stuff </p>
               </div>
             </div>
             <div className="row">
-              <div className="col-md-12 text-center">
+              <div className="col-md-12 text-center landing-row3">
                 <h1 className="display-3 mb-4">THIRD ROW</h1>
                 <p className="lead"> third row stuff </p>
               </div>
             </div>
             <div className="row">
-              <div className="col-md-12 text-center">
+              <div className="col-md-12 text-center landing-row4">
                 <h1 className="display-3 mb-4">FOURTH ROW</h1>
                 <p className="lead"> fourth row stuff </p>
               </div>
@@ -69,20 +69,22 @@ Landing.propTypes = {
   stories: PropTypes.shape({
     filterBy: PropTypes.shape({
       topStories: PropTypes.shape({
-        items: PropTypes.arrayOf(PropTypes.shape({
-          author: PropTypes.string.isRequired,
-          city: PropTypes.string.isRequired,
-          county: PropTypes.string.isRequired,
-          _id: PropTypes.string.isRequired,
-          nominations: PropTypes.number.isRequired,
-          photo_url: PropTypes.string.isRequired,
-          state: PropTypes.string.isRequired,
-          tag: PropTypes.string.isRequired,
-          text: PropTypes.string.isRequired,
-          title: PropTypes.string.isRequired,
-          upvotes: PropTypes.number.isRequired,
-          zipcode: PropTypes.number.isRequired,
-        })),
+        items: PropTypes.arrayOf(
+          PropTypes.shape({
+            author: PropTypes.string.isRequired,
+            city: PropTypes.string.isRequired,
+            county: PropTypes.string.isRequired,
+            _id: PropTypes.string.isRequired,
+            nominations: PropTypes.number.isRequired,
+            photo_url: PropTypes.string.isRequired,
+            state: PropTypes.string.isRequired,
+            tag: PropTypes.string.isRequired,
+            text: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            upvotes: PropTypes.number.isRequired,
+            zipcode: PropTypes.number.isRequired,
+          })
+        ),
       }),
     }).isRequired,
   }).isRequired,
@@ -95,7 +97,7 @@ const mapDispatchToProps = dispatch =>
       fetchStories,
       updateVote,
     },
-    dispatch,
+    dispatch
   );
 
 const mapStateToProps = state => ({
@@ -105,5 +107,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(Landing);
